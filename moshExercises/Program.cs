f0ea1b31e44e9a2fbe3c0aa78a6f1f30604e20b9
Mattias Öigaard <mattias.oigaard@gmail.com>
2019-10-25 07:15:46 +0200

    Add project files.

---
+++ moshExercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace moshExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number between 1 and 10: ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+        static void Exercise2()
+        {
+
+        }
+    }
+}
