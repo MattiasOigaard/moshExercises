bfd4e8147936dc74f0f800355ea86ea6454f009f
Mattias Öigaard <mattias.oigaard@gmail.com>
2019-10-25 07:36:39 +0200

    moshExercises ver2

--- moshExercises/Program.cs
+++ moshExercises/Program.cs
@@ -6,26 +6,17 @@ namespace moshExercises
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter a number between 1 and 10: ");
-            string input = Console.ReadLine();
-            int number = Convert.ToInt32(input);
-            if (number >= 1 && number <= 10)
-            {
-                Console.WriteLine("Valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid");
-            }
-        }
-
-        static void Exercise2()
-        {
             Console.Write("Enter a number: ");
             int number1 = Convert.ToInt32(Console.ReadLine());
 
             Console.Write("Enter another number: ");
             int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("Max is " + max);
+
         }
+
+        
     }
 }
